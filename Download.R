bda9a338e702f99518229ad2fe1b7296745a3743
CBMontminy <19cbm1@queensu.ca>
2023-02-13 18:35:54 -0500

    Script that creates Sequence dataframe.

---
+++ Download.R
@@ -0,0 +1,13 @@
+ncbi_ids <- c("HQ433692.1","HQ433694.1","HQ433691.1") # creates a list of 3 IDs
+library(rentrez)  # loading rentrez package
+Bburg<-entrez_fetch(db = "nuccore", 
+                    id = ncbi_ids, 
+                    rettype = "fasta") # Creates Bburg object from the nuccore database and loads as a FASTA file.
+str(Bburg)
+
+Sequences=strsplit(Bburg, split="\n\n") # Splits the sequences by the double return "\n\n"
+Sequences=unlist(Sequences) # Unlisting so we can convert to a dataframe
+
+Header=gsub("(^>.*sequence)\\n[ATCG].*", "\\1", Sequences)
+Seq=gsub("^>.*sequence\\n([ATCG].*)", "\\1", Sequences)
+Sequences=data.frame(Name=Header, Sequence=Seq)
